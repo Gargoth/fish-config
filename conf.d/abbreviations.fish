d5f039da165ade8fe72d9c73e9b8418509d85919
CJ Rosales <gargoth13@gmail.com>
2024-06-24 23:51:34 +0800

    feat: Added abbreviations for tmux

--- conf.d/abbreviations.fish
+++ conf.d/abbreviations.fish
@@ -1,9 +1,11 @@
-# Abbreviations
+# eza
 abbr --add ls 'eza --color=always --group-directories-first --icons'
 abbr --add la 'eza -a --color=always --group-directories-first --icons'
 abbr --add ll 'eza -l --color=always --group-directories-first --icons'
 abbr --add lt 'eza -T --color=always --group-directories-first --icons --ignore-glob="node_modules|venv"'
 abbr --add ldir 'eza -d --color=always --group-directories-first --icons'
+
+# git
 abbr --add gs 'git status'
 abbr --add ga 'git add'
 abbr --add ga. 'git add .'
@@ -15,7 +17,11 @@ abbr --add gd 'git diff'
 abbr --add gr 'git restore'
 abbr --add gl "git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all"
 abbr --add gswc 'git switch -c'
-abbr --add sfish 'source ~/.config/fish/config.fish'
-abbr --add .. 'cd ..'
-abbr --add ... 'cd ...'
-abbr --add .... 'cd ....'
+
+# fish
+abbr --add sfish 'source ~/.config/fish/**/*.fish'
+
+# tmux
+abbr --add tm 'tmux'
+abbr --add tma 'tmux a'
+abbr --add tmks 'tmux kill-server'
