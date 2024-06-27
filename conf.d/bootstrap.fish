09678ae91f66546792b084ad760c8aab9719bdc7
Christian Jay F. Rosales <gargoth13@gmail.com>
2024-06-27 19:31:46 +0800

    Removed fisher

--- conf.d/bootstrap.fish
+++
@@ -1,4 +0,0 @@
-# Bootstrap fisher plugin manager
-if not type -q fisher
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-end
