3d4d2d9ad75f5824422d1eeb1c9a9edbcbb50f5d
CJ Rosales <gargoth13@gmail.com>
2024-10-27 16:32:41 +0800

    feat: Added activated venv to prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt --description Hydro
-  # echo -e "$_hydro_color_beginning$hydro_prefix_beginning$_hydro_color_pwd$hydro_prefix_pwd$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
-    echo -e "$_hydro_color_pwd$hydro_prefix_pwd$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
+  if test -n "$VIRTUAL_ENV_PROMPT"
+    set -f venv_prompt "($VIRTUAL_ENV_PROMPT) "
+  end
+    echo -e "$_hydro_color_pwd$hydro_prefix_pwd$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$venv_prompt$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_status$hydro_color_normal "
 end
